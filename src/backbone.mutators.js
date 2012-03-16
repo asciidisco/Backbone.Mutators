88b6308dff6ab0d8846317887cd528421bfa3c9a
asciidisco <public@asciidisco.com>
2012-03-16 14:44:44 +0100

    Added submodule dependencies

---
+++ src/backbone.mutators.js
@@ -0,0 +1,107 @@
+/*jslint nomen: true, indent: 4, es5: false, length: 120 */
+
+// Backbone.Mutators v0.1.0
+//
+//
+// Documentation and Full License Available at:
+// http://github.com/asciidisco/Backbone.Mutators
+
+// Usage:
+//
+// Note: This plugin is UMD compatible, you can use it in node, amd and vanilla js envs
+//
+// Vanilla JS:
+// <script src="underscore.js"></script>
+// <script src="backbone.js"></script>
+// <script src="backbone.mutators.js"></script>
+//
+// AMD:
+// define(['underscore', 'backbone', 'backbone.mutators'], function (_, Backbone, Mutators) {
+//     _.extend(Backbone.Model.prototype, Mutators.prototype);
+//    // insert sample from below
+//    return User;
+//
+// HINT: If you use some plugin like Backbone.Relational you need to do smth. like this:
+// _.extend(Backbone.RelationalModel.prototype, Mutators.prototype);
+// });
+//
+// var User = Backbone.Model.extend({
+//    mutators: {
+//        fullname: function () {
+//            return this.forename + ' ' + this.lastname;
+//        }
+//    },
+//
+//    defaults: {
+//        forename: 'Sebastian',
+//        lastname: 'Golasch'
+//    }
+// });
+//
+// var user = new User();
+// user.get('fullname') // returns 'Sebastian Golasch'
+// user.toJSON() // return '{surename: 'Sebastian', lastname: 'Golasch', fullname: 'Sebastian Golasch'}'
+
+(function(root, factory) {
+  'use strict';
+
+  // Set up Backbone appropriately for the environment.
+  if (typeof exports !== 'undefined') {
+    // Node/CommonJS
+    factory(root, exports, require('underscore'));
+  } else if (typeof define === 'function' && define.amd) {
+    // AMD
+    define('mutators', ['underscore', 'backbone', 'exports'], function(root, _, Backbone, exports) {
+      return factory(root, exports, _, Backbone);
+    });
+  } else {
+    // Browser globals
+    root.Backbone = factory(root, {}, root._);
+  }
+}(this, function(root, _, Backbone, exports, undef) {
+    'use strict';
+
+    // check if we use the amd branch of backbone and underscore
+    Backbone = Backbone === undef ? window.Backbone : Backbone;
+    _ = _ === undef ? window._ : _;
+
+    // extend backbones model prototype with the mutator functionality
+    var Mutator     = function () {},
+        oldGet      = Backbone.Model.prototype.get,
+        oldSet      = Backbone.Model.prototype.set,
+        oldToJson   = Backbone.Model.prototype.toJSON;
+
+    // override get functionality to fetch the mutator props
+    Mutator.prototype.get = function (attr) {
+        var isMutator = this.mutators !== undef;
+
+        // check if we have a getter mutation
+        if (isMutator === true && typeof this.mutators[attr] === 'function') {
+            return _.bind(this.mutators[attr], this.attributes)();
+        }
+
+        // check if we have a deeper nested getter mutation
+        if (isMutator === true && typeof this.mutators[attr] === 'object' && _.isArray(this.mutators[attr]) === false) {
+            return _.bind(this.mutators[attr], this.attributes)();
+        }
+
+        return oldGet.call(this, attr);
+    };
+
+    // override get functionality to set the mutator props
+    Mutator.prototype.set = function (attr) {
+        return oldSet.call(this, attr);
+    };
+
+    // override toJSON functionality to serialize mutator properties
+    Mutator.prototype.toJSON = function () {
+        var attr = oldToJson.call(this);
+        _.each(this.mutators, _.bind(function (mutator, name) {
+            attr[name] = _.bind(this.mutators[name], this.attributes)();
+        }, this));
+
+        return attr;
+    };
+
+    return Mutator;
+}));
---
+++ test/tests.js
@@ -0,0 +1,7 @@
+module('Backbone.Mutators');
+
+test("initialize", function () {
+  expect(1);
+  this.errors = [];
+  ok(true, 'It worked if it ends with ok')
+});
