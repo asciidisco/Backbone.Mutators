969a9e2e5f38e66ecb8c4948934b77f545e29729
asciidisco <public@asciidisco.com>
2013-04-30 22:11:45 +0200

    Moved to grunt 0.4, added node tests

---
+++ Gruntfile.js
@@ -0,0 +1,100 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    jshint: {
+      options: {
+        jshintrc:'.jshintrc'
+      },
+      all: ['Gruntfile.js', 'src/*.js', 'test/*.js']
+    },
+
+    qunit: {
+      options: {
+        '--web-security':'no',
+        timeout: 40000,
+        coverage: {
+          src: ['src/*.js'],
+          instrumentedFiles: 'temp',
+          htmlReport: 'report/coverage'
+        }
+      },
+      all: ['test/**/*.html']
+    },
+
+    nodeunit: {
+      all: ['test/*node_test.js']
+    },
+
+    concat: {
+      options: {
+        stripBanners: true,
+        banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %>\n' +
+          '------------------------------\n' +
+          'Build @ <%= grunt.template.today("yyyy-mm-dd") %>\n' +
+          'Documentation and Full License Available at:\n' +
+          '<%= pkg.homepage %>\n' +
+          '<%= pkg.repository.url %>\n' +
+          'Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n\n' +
+          'Permission is hereby granted, free of charge, to any person obtaining a\n' +
+          'copy of this software and associated documentation files (the "Software"),\n' +
+          'to deal in the Software without restriction, including without limitation\n' +
+          'the rights to use, copy, modify, merge, publish, distribute, sublicense,\n' +
+          'and/or sell copies of the Software, and to permit persons to whom the\n\n' +
+          'Software is furnished to do so, subject to the following conditions:\n' +
+          'The above copyright notice and this permission notice shall be included in\n' +
+          'all copies or substantial portions of the Software.\n\n' +
+          'THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,\n' +
+          'EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n' +
+          'FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.\n' +
+          'IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,\n' +
+          'DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,\n' +
+          'ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS\n' +
+          'IN THE SOFTWARE.*/\n'
+      },
+      dist: {
+        src: ['src/<%= pkg.name.toLowerCase() %>.js'],
+        dest: '<%= pkg.name.toLowerCase() %>.js'
+      }
+    },
+
+    uglify: {
+      options: {
+        banner: '<%= concat.options.banner %>'
+      },
+      dist: {
+        files: {
+          '<%= pkg.name.toLowerCase() %>.min.js': ['<%= pkg.name.toLowerCase() %>.js']
+        }
+      }
+    },
+
+    docco: {
+      debug: {
+        src: ['src/*.js'],
+        options: {
+          output: 'docs/'
+        }
+      }
+    }
+
+  });
+
+  // Load 3rd party tasks
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-qunit-istanbul');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-contrib-yuidocs');
+  grunt.loadNpmTasks('grunt-docco');
+  grunt.loadNpmTasks('grunt-plato');
+
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'qunit', 'nodeunit', 'concat', 'uglify']);
+  grunt.registerTask('travis', 'qunit lint');
+
+};
---
+++ test/node_test.js
@@ -0,0 +1,450 @@
+var _ = require('underscore');
+var Backbone = require('Backbone');
+require('../src/backbone.mutators');
+
+exports['require'] = {
+
+    "can get 'normal' value": function (test) {
+        test.expect(2);
+        var Model = Backbone.Model.extend({
+            mutators: {
+                fullname: function () {
+                    return this.firstname + ' ' + this.lastname;
+                }
+            }
+        });
+
+        var model = new Model();
+        model.set('firstname', 'Sebastian');
+        model.set('lastname', 'Golasch');
+
+        test.equal(model.get('firstname'), 'Sebastian', 'Can get unmutated firstname');
+        test.equal(model.get('lastname'), 'Golasch', 'Can get unmutated lastname');
+        test.done();
+    },
+
+    "can get 'mutated' value (newly created)": function (test) {
+        test.expect(3);
+        var Model = Backbone.Model.extend({
+            mutators: {
+                fullname: function () {
+                    return this.get('firstname') + ' ' + this.get('lastname');
+                }
+            }
+        });
+
+        var model = new Model();
+        model.set('firstname', 'Sebastian');
+        model.set('lastname', 'Golasch');
+        test.equal(model.get('firstname'), 'Sebastian', 'Can get unmutated firstname');
+        test.equal(model.get('lastname'), 'Golasch', 'Can get unmutated lastname');
+        test.equal(model.get('fullname'), 'Sebastian Golasch', 'Can get mutated fullname');
+        test.done();
+    },
+
+    "can get 'mutated' value (overridden)": function (test) {
+        test.expect(5);
+        var Model = Backbone.Model.extend({
+            mutators: {
+                status: function (key, value, options, set) {
+                    if(key){
+                        set('status', value);
+                    }
+
+                    return { status: this.attributes.status, overallStatus: this.get('overallStatus'), underestimatedNonOverallStatus: this.get('underestimatedNonOverallStatus') };
+                }
+            }
+        });
+
+        var model = new Model();
+        model.set('overallStatus', 1);
+        model.set('underestimatedNonOverallStatus', 3);
+        model.set('status', 2);
+
+        test.equal(model.get('overallStatus'), 1, 'Can get unmutated overallStatus');
+        test.equal(model.get('underestimatedNonOverallStatus'), 3, 'Can get unmutated underestimatedNonOverallStatus');
+        test.equal(model.get('status').status, 2, 'Can get mutated status');
+        test.equal(model.get('status').overallStatus, 1, 'Can get mutated status');
+        test.equal(model.get('status').underestimatedNonOverallStatus, 3, 'Can get mutated status');
+        test.done();
+    },
+
+    "can get 'normal' value - object context": function (test) {
+        test.expect(2);
+        var Model = Backbone.Model.extend({
+            mutators: {
+                fullanme: {
+                    get: function () {
+                        return this.get('firstname') + ' ' + this.get('lastname');
+                    }
+                }
+            }
+        });
+
+        var model = new Model();
+        model.set('firstname', 'Sebastian');
+        model.set('lastname', 'Golasch');
+
+        test.equal(model.get('firstname'), 'Sebastian', 'Can get unmutated firstname');
+        test.equal(model.get('lastname'), 'Golasch', 'Can get unmutated lastname');
+        test.done();
+    },
+
+    "can get 'mutated' value (newly created) - object context": function (test) {
+        test.expect(3);
+        var Model = Backbone.Model.extend({
+            mutators: {
+                fullname: {
+                    get: function () {
+                        return this.get('firstname') + ' ' + this.get('lastname');
+                    }
+                }
+            }
+        });
+
+        var model = new Model();
+        model.set('firstname', 'Sebastian');
+        model.set('lastname', 'Golasch');
+
+        test.equal(model.get('firstname'), 'Sebastian', 'Can get unmutated firstname');
+        test.equal(model.get('lastname'), 'Golasch', 'Can get unmutated lastname');
+        test.equal(model.get('fullname'), 'Sebastian Golasch', 'Can get mutated fullname');
+        test.done();
+    },
+
+    "can get 'mutated' value (overridden) - object context": function (test) {
+        test.expect(5);
+        var Model = Backbone.Model.extend({
+            mutators: {
+                status: {
+                    get: function () {
+                        return { status: this.attributes.status, overallStatus: this.get('overallStatus'), underestimatedNonOverallStatus: this.get('underestimatedNonOverallStatus') };
+                    }
+                }
+            }
+        });
+
+        var model = new Model();
+        model.set('overallStatus', 1);
+        model.set('underestimatedNonOverallStatus', 3);
+        model.set('status', 2);
+
+        test.equal(model.get('overallStatus'), 1, 'Can get unmutated overallStatus');
+        test.equal(model.get('underestimatedNonOverallStatus'), 3, 'Can get unmutated underestimatedNonOverallStatus');
+        test.equal(model.get('status').status, 2, 'Can get mutated status');
+        test.equal(model.get('status').overallStatus, 1, 'Can get mutated status');
+        test.equal(model.get('status').underestimatedNonOverallStatus, 3, 'Can get mutated status');
+        test.done();
+    },
+
+    "can set 'normal' value (key <-> value)": function (test) {
+        test.expect(1);
+        var Model = Backbone.Model.extend({
+            mutators: {
+                status: {
+                    set: function () {
+                        return { status: this.attributes.status, overallStatus: this.get('overallStatus'), underestimatedNonOverallStatus: this.get('underestimatedNonOverallStatus') };
+                    }
+                }
+            }
+        });
+
+        var model = new Model();
+        model.set('overallStatus', 1);
+
+        test.equal(model.get('overallStatus'), 1, 'Can get unmutated overallStatus');
+        test.done();
+    },
+
+    "can set 'mutated' value (key <-> value)": function (test) {
+        test.expect(2);
+        var Model = Backbone.Model.extend({
+            mutators: {
+                status: {
+                    set: function (key, value, options) {
+                        this.set('pState', value.pState, options);
+                        this.set('aState', value.aState, options);
+                    }
+                }
+            }
+        });
+
+        var model = new Model();
+        model.set('status', {pState: 1, aState: 2});
+
+        test.equal(model.get('pState'), 1, 'Can get mutated pState');
+        test.equal(model.get('aState'), 2, 'Can get mutated aState');
+        test.done();
+    },
+
+    "can set 'normal' value (object)": function (test) {
+        test.expect(1);
+        var Model = Backbone.Model.extend({
+            mutators: {
+                status: {
+                    set: function () {
+                        return { status: this.attributes.status, overallStatus: this.get('overallStatus'), underestimatedNonOverallStatus: this.get('underestimatedNonOverallStatus') };
+                    }
+                }
+            }
+        });
+
+        var model = new Model();
+        model.set({overallStatus: 1});
+
+        test.equal(model.get('overallStatus'), 1, 'Can get unmutated overallStatus');
+        test.done();
+    },
+
+    "can set attribute objects": function (test) {
+        test.expect(3);
+        var Model = Backbone.Model.extend({
+            mutators: {
+                fullname: {
+                    set: function (key, value, options) {
+                        var names = value.split(' ');
+                        this.set('firstname', names[0], options);
+                        this.set('lastname', names[1], options);
+                    }
+                }
+            }
+        });
+
+        var model = new Model();
+        model.set({ fullname: 'Sebastian Golasch', admin: true });
+
+        test.equal(model.get('firstname'), 'Sebastian', 'Can get the firstname');
+        test.equal(model.get('lastname'), 'Golasch', 'Can get the lastname');
+        test.equal(model.get('admin'), true, 'Can get the admin status');
+        test.done();
+    },
+
+    "can set newly created 'mutated' value (object)": function (test) {
+        test.expect(2);
+        var Model = Backbone.Model.extend({
+            mutators: {
+                status: {
+                    set: function (key, value, options) {
+                        this.set('pState', value.pState, options);
+                        this.set('aState', value.aState, options);
+                    }
+                }
+            }
+        });
+
+        var model = new Model();
+        model.set({status: {pState: 1, aState: 2, dState: 3}});
+
+        test.equal(model.get('pState'), 1, 'Can get mutated pState');
+        test.equal(model.get('aState'), 2, 'Can get mutated aState');
+        test.done();
+    },
+
+    "can set 'mutated' value (object)": function (test) {
+        test.expect(4);
+        var Model = Backbone.Model.extend({
+            mutators: {
+                status: {
+                    set: function (key, value, options, set) {
+                        if(_.isString(value)){
+                            set(key, value);
+                        } else {
+                            this.set('pState', value.pState, options);
+                            this.set('aState', value.aState, options);
+                            if (value.pState === 1) {
+                                set(key, 'supercool', options);
+                            }
+                        }
+                    }
+                }
+            },
+            defaults: {
+                status: 'awkward'
+            }
+        });
+
+        var model = new Model();
+
+        test.equal(model.get('status'), 'awkward', 'Can get unmodified value');
+        model.set({status: {pState: 1, aState: 2, dState: 3}});
+        test.equal(model.get('status'), 'supercool', 'Can get mutated status value');
+        test.equal(model.get('pState'), 1, 'Can get mutated pState');
+        test.equal(model.get('aState'), 2, 'Can get mutated aState');
+        test.done();
+    },
+
+    "can set a 'mutated' value and fire event": function (test) {
+        test.expect(3);
+        var Model = Backbone.Model.extend({
+            mutators: {
+                status: {
+                    set: function (key, value, options, set) {
+                        set(key, value.toLowerCase(), options);
+                    }
+                }
+            },
+            defaults: {
+                status: 'awkward'
+            }
+        });
+
+        var model = new Model();
+
+        model.bind('mutators:set:status', function () {
+            test.ok(true, 'Callback called');
+        });
+
+        test.equal(model.get('status'), 'awkward', 'Can get unmodified value');
+        model.set({status: 'SUPERCOOL'});
+        test.equal(model.get('status'), 'supercool', 'Can get mutated status value');
+        test.done();
+    },
+
+    "can set 'mutated' value and fire a mutated event": function (test) {
+        test.expect(2);
+        var Model = Backbone.Model.extend({
+            mutators: {
+                status: {
+                    set: function (key, value, options, set) {
+                        set(key, value.toLowerCase(), options);
+                    }
+                }
+            },
+            defaults: {
+                status: 'awkward'
+            }
+        });
+
+        var model = new Model();
+
+        model.bind('mutators:set:status', function () {
+            test.ok(true, 'Callback called (And this shouldn´t happen)');
+        });
+
+        test.equal(model.get('status'), 'awkward', 'Can get unmodified value');
+        model.set('status', 'SUPERCOOL', {silent: true});
+        test.equal(model.get('status'), 'supercool', 'Can get mutated status value');
+        test.done();
+    },
+
+    "can set 'mutated' value and fire event": function (test) {
+        test.expect(3);
+        var Model = Backbone.Model.extend({
+            mutators: {
+                status: {
+                    set: function (key, value, options, set) {
+                        set(key, value.toLowerCase(), options);
+                    }
+                }
+            },
+            defaults: {
+                status: 'awkward'
+            }
+        });
+
+        var model = new Model();
+
+        model.bind('mutators:set:status', function () {
+            test.ok(true, 'Callback called (And this shouldn´t happen)');
+        });
+
+        model.bind('change:status', function () {
+            test.ok(true, 'Callback called (And this should happen)');
+        });
+
+        test.equal(model.get('status'), 'awkward', 'Can get unmodified value');
+        model.set('status', 'SUPERCOOL', {mutators: {silent: true}});
+        test.equal(model.get('status'), 'supercool', 'Can get mutated status value');
+        test.done();
+    },
+
+    "can serialize an unmutated model": function (test) {
+        test.expect(2);
+        var Model = Backbone.Model.extend({
+            defaults: {
+                a: 'a',
+                b: 'b'
+            }
+        });
+
+        test.equal((new Model()).toJSON().a, 'a', 'can serialize unmutated model');
+        test.equal((new Model()).toJSON().b, 'b', 'can serialize unmutated model');
+        test.done();
+    },
+
+    "can serialize mutated model": function (test) {
+        test.expect(3);
+        var Model = Backbone.Model.extend({
+            defaults: {
+                a: 'a',
+                b: 'b'
+            },
+            mutators: {
+                state: function () {
+                    return this.get('a') + ', ' + this.get('b');
+                }
+            }
+        });
+
+        test.equal((new Model()).toJSON().a, 'a', 'can serialize mutated model');
+        test.equal((new Model()).get('state'), 'a, b', 'can serialize mutated model');
+        test.equal((new Model()).toJSON().state, 'a, b', 'can serialize mutated model');
+        test.done();
+    },
+
+    "can escape mutated properties": function (test) {
+        test.expect(2);
+        var Model = Backbone.Model.extend({
+            defaults: {
+                a: 'a',
+                b: 'b'
+            },
+            mutators: {
+                b: function () {
+                    return 'c';
+                }
+            }
+        });
+
+        var model = new Model();
+        test.equal(model.get('b'), 'c');
+        model.set('b', 'foobar');
+        test.equal(model.get('b'), 'c');
+        test.done();
+    },
+
+    "can get/set using single method": function (test) {
+        test.expect(6);
+        var Model = Backbone.Model.extend({
+            mutators:{
+                state:function(key, value){
+                    if(key){
+                        this.set("a", value);
+                        test.equal(arguments.length, 4);
+                        return null; //prevents ret from returning
+                    }
+                    return this.get("a");
+                }
+            }
+        });
+
+        var model = new Model();
+        var value = "happy";
+        model.set('state', value);
+
+        test.equal(model.get('state'), value);
+        var new_state = "excited",
+        new_level = 10;
+
+        //set multiple
+        model.set({
+            level:new_level,
+            state:new_state
+        });
+
+        test.equal(model.get('state'), new_state);
+        test.equal(model.get('level'), new_level);
+        test.done();
+    }
+
+};
