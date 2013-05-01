2b0eeca0748ef39a46faee3155bd632c425d6259
asciidisco <public@asciidisco.com>
2013-05-01 13:47:59 +0200

    Fixing Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -114,4 +114,5 @@ module.exports = function(grunt) {
   grunt.registerTask('default', ['jshint', 'qunit', 'nodeunit', 'concat', 'uglify']);
   grunt.registerTask('travis', ['jshint', 'qunit', 'saucelabs-qunit']);
 
+
 };
